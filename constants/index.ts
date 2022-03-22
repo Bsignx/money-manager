20ade613591e1195fc9eb41d3bd31ed87262d809
Bruno Mariano <bsignx@gmail.com>
2022-03-22 16:43:05 -0300

    chore: create transaction type options constant

---
+++ constants/index.ts
@@ -0,0 +1,12 @@
+const TRANSACTION_TYPE_OPTIONS = [
+  {
+    optionId: "INCOME",
+    displayText: "Income",
+  },
+  {
+    optionId: "EXPENSE",
+    displayText: "Expense",
+  },
+];
+
+export { TRANSACTION_TYPE_OPTIONS };
